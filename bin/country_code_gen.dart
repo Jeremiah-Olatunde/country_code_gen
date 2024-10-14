9d820fe33ed30991700aab336426dc45c62d24e3
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-14 23:59:40 +0100

    chore(setup): initial commit

---
+++ bin/country_code_gen.dart
@@ -0,0 +1,5 @@
+import 'package:country_code_gen/country_code_gen.dart' as country_code_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${country_code_gen.calculate()}!');
+}
---
+++ test/country_code_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:country_code_gen/country_code_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
